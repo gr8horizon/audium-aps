3b91622a4ae76a553b5a81be312c014ba34d1bac
Paul Barton <gr8horizon@gmail.com>
2022-03-01 21:36:01 -0800

    Update aps_install.sh
    show status

--- aps_install.sh
+++ aps_install.sh
@@ -3,10 +3,13 @@
 # Installs aps.service on raspberry-pi-zero
 # then restarts service
 
+echo Installing APS.service ...
+
 sudo cp aps.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/aps.service
 sudo systemctl daemon-reload
 sudo systemctl enable aps.service
 sudo systemctl restart aps.service
+sudo systemctl status aps.service
 
-echo APS.service installed.
+echo Done.
